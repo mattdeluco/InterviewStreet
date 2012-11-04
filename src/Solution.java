ba61144f389a7fb98b6cf6ba8a161578263b114c
Matt DeLuco <matt@deluco.net>
2012-11-04 18:58:30 -0500

    Submission 3 to Challenges/Insertion Sort.  Passed 4/10, time limit exceeded.

--- src/Solution.java
+++ src/Solution.java
@@ -2,23 +2,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
 
 public class Solution {
 
 	private static List<List<Integer>> testList;
 	
 	public static void countSwaps(List<Integer> list) {
-		int swaps = 0;
+		
+		int totalSwaps = 0;
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 		
 		for (int i = 1 ; i < list.size() ; i++) {
+			int swaps = 0;
+			
+			if (map.containsKey(list.get(i)))
+				swaps += map.get(list.get(i));
+			
 			for (int j = i ; j > 0 ; j--) {
 				if (list.get(i).compareTo(list.get(j-1)) < 0)
 					swaps++;
+				else if (list.get(i).compareTo(list.get(j-1)) == 0)
+					break;
 			}
+			
+			totalSwaps += swaps;
+			map.put(list.get(i), swaps);
 		}
 		
-		System.out.println(swaps);
+		System.out.println(totalSwaps);
+		
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -35,7 +53,7 @@ public class Solution {
 			} else {
 				List<Integer> list = testList.get((lineNum/2) - 1);
 				for (String i : line.split(" ")) {
-					list.add(Integer.parseInt(i));
+					list.add(new Integer(Integer.parseInt(i)));
 				}
 			}
 		}
