3072f7ba76d00753addda2ba52000132b2c23b3f
Matt DeLuco <matt@deluco.net>
2012-11-04 23:14:41 -0500

    Submission 5 to Challenges/Insertion Sort.  Result: Time limit exceeeded.  4/10 testcases passed.  Score 31.36.

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,9 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
 
 public class Solution {
@@ -20,24 +19,22 @@ public class Solution {
 			
 			int totalCount = 0;
 			String[] values = lnr.readLine().split(" ");
-			List<Integer> list = new LinkedList<Integer>();
+			List<Integer> list = new ArrayList<Integer>(values.length);
 			Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 			
 			for (int i = 0; i < values.length && i < numValues; i++) {
 				
 				Integer v = Integer.parseInt(values[i]);
 				Integer swaps = map.containsKey(v) ? map.get(v) : new Integer(0);
+				list.add(v);
 				
-				ListIterator<Integer> iter = list.listIterator(list.size());
-				while (iter.hasPrevious()) {
-					Integer p = iter.previous();
-					if (v.compareTo(p) < 0)
+				for (int j = i; i > 0 && j > 0; j--) {
+					if (v.compareTo(list.get(j-1)) < 0)
 						swaps++;
-					else if (v.compareTo(p) == 0)
+					else if (v.compareTo(list.get(j-1)) == 0)
 						break;
 				}
 				
-				list.add(v);
 				map.put(v, swaps);
 				totalCount += swaps;
 				
