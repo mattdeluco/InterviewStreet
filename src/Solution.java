fcecd00694a931e846edd5fe3100cb67c0c23b36
Matt DeLuco <matt@deluco.net>
2012-11-06 23:06:53 -0500

    Solution 6 to Challenges/Insertion Sort.  Unsubmitted.

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,7 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.TreeSet;
 
 public class Solution {
 	
@@ -16,27 +13,16 @@ public class Solution {
 		while (numTests-- > 0) {
 			
 			Integer numValues = Integer.parseInt(lnr.readLine());
+			String[] values = lnr.readLine().split(" ");
 			
 			int totalCount = 0;
-			String[] values = lnr.readLine().split(" ");
-			List<Integer> list = new ArrayList<Integer>(values.length);
-			Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+			TreeSet<Integer> set = new TreeSet<Integer>();
 			
 			for (int i = 0; i < values.length && i < numValues; i++) {
 				
 				Integer v = Integer.parseInt(values[i]);
-				Integer swaps = map.containsKey(v) ? map.get(v) : new Integer(0);
-				list.add(v);
-				
-				for (int j = i; i > 0 && j > 0; j--) {
-					if (v.compareTo(list.get(j-1)) < 0)
-						swaps++;
-					else if (v.compareTo(list.get(j-1)) == 0)
-						break;
-				}
-				
-				map.put(v, swaps);
-				totalCount += swaps;
+				totalCount += set.tailSet(v, false).size();
+				set.add(v);
 				
 			}
 			
