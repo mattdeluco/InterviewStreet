064c874ab666da13a61a5597bfd67ab56db9ca64
Matt DeLuco <matt@deluco.net>
2012-11-08 11:49:13 -0500

    Solution 7 to Challenges/Insertion Sort.  Result: Wrong Answer.  7/10 testcases passed.  Score 62.72.

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,52 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
-import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
+	
+	private static Integer swapCount;
+	
+	public static List<String> merge(List<String> left, List<String> right) {
+		
+		List<String> list = new ArrayList<String>(left.size() + right.size());
+		int lIdx = 0, rIdx = 0;
+		
+		while (lIdx < left.size() || rIdx < right.size()) {
+			if (lIdx < left.size() && rIdx < right.size()) {
+				Integer l = Integer.parseInt(left.get(lIdx));
+				Integer r = Integer.parseInt(right.get(rIdx));
+				if (l > r) {
+					list.add(right.get(rIdx++));
+					swapCount += left.size() - lIdx;
+				} else {
+					list.add(left.get(lIdx++));
+				}
+			} else if (lIdx < left.size()) {
+				list.add(left.get(lIdx++));
+			} else if (rIdx < right.size()) {
+				list.add(right.get(rIdx++));
+			}
+		}
+		
+		return list;
+		
+	}
+	
+	public static List<String> mergeSort(List<String> list) {
+		if (list.size() <= 1) return list;
+		
+		List<String> left = list.subList(0,  list.size()/2);
+		List<String> right = list.subList(list.size()/2, list.size());
+		
+		left = mergeSort(left);
+		right = mergeSort(right);
+		
+		return merge(left, right);
+	}
+	
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -11,23 +54,14 @@ public class Solution {
 		int numTests = Integer.parseInt(lnr.readLine());
 		
 		while (numTests-- > 0) {
+			swapCount = 0;
 			
 			Integer numValues = Integer.parseInt(lnr.readLine());
-			String[] values = lnr.readLine().split(" ");
+			List<String> values = new ArrayList<String>(Arrays.asList(lnr.readLine().split("\\s+")));
 			
-			int totalCount = 0;
-			TreeSet<Integer> set = new TreeSet<Integer>();
+			List<String> list = mergeSort(values);
 			
-			for (int i = 0; i < values.length && i < numValues; i++) {
-				
-				Integer v = Integer.parseInt(values[i]);
-				totalCount += set.tailSet(v, false).size();
-				set.add(v);
-				
-			}
-			
-			System.out.println(totalCount);
-			
+			System.out.println(swapCount);
 		}
 		
 	}
